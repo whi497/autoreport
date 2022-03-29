06afb77ebe0efaaafba53710ee07f1ca4a1c73d6
my leaf <75080416+whi497@users.noreply.github.com>
2022-03-29 15:08:35 +0800

    Update autoreport.py

--- autoreport.py
+++ autoreport.py
@@ -5,6 +5,7 @@ import argparse
 import time
 
 chrome_options = webdriver.ChromeOptions()
+chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_argument('--disable-gpu')
 chrome_options.add_argument('--disable-dev-shm-usage')
