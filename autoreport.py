8390dbd4857cb3df2fc7c3fad42590cb90ac8fc2
my leaf <75080416+whi497@users.noreply.github.com>
2022-03-29 14:48:06 +0800

    add

--- autoreport.py
+++ autoreport.py
@@ -1,7 +1,13 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 import argparse
 import time
+
+chrome_options = Options()
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 
 class Report(object):
     def __init__(self,username,password):
